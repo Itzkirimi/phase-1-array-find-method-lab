5ec2b9717638b0117911c1ffe21240748e0f1500
Itzkirimi <kirimihansel@gmail.com>
2024-01-15 00:03:07 +0300

    Done with Assignments

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(record) {
+    const winObject = record.find(entry => entry.result === "W");
+  
+    if (winObject) {
+      return winObject.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  const record = [
+  ];
+  
+  console.log(superbowlWin(record)); // Output: '2015'
+  
